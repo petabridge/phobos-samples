4130f2c826bf6b2cef510e87024f65b41f54ac2f
Aaron Stannard <aaron@petabridge.com>
2019-02-18 12:36:27 -0600

    pushed rest of updated sample

--- Phobos.Samples.ClusterPing/RandomBroadcastActor.cs
+++ Phobos.Samples.ClusterPing/RandomBroadcastActor.cs
@@ -5,11 +5,10 @@
 // -----------------------------------------------------------------------
 
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Linq;
 using Akka.Actor;
-using Akka.Cluster;
 using Akka.Event;
+using Akka.Routing;
 using Akka.Util;
 using Phobos.Actor;
 
@@ -18,62 +17,15 @@ namespace Phobos.Samples.ClusterPing
     /// <summary>
     ///     Randomly broadcasts messages out to other member nodes in the cluster upon receiving a ping
     /// </summary>
-    public sealed class RandomBroadcastActor : ReceiveActor, IWithUnboundedStash
+    public sealed class RandomBroadcastActor : ReceiveActor
     {
         private readonly Akka.Cluster.Cluster _cluster = Akka.Cluster.Cluster.Get(Context.System);
         private readonly ILoggingAdapter _log = Context.GetLogger();
-        private readonly HashSet<IActorRef> _neighbors = new HashSet<IActorRef>();
+        private IActorRef _router;
 
         public RandomBroadcastActor()
         {
-            WaitingClusterUp();
-        }
-
-        public IStash Stash { get; set; }
-
-        public void WaitingClusterUp()
-        {
-            ReceiveAsync<ClusterEvent.MemberWeaklyUp>(async m =>
-            {
-                await AddMember(m.Member);
-                BecomeTransmitting();
-            }, m => !m.Member.Address.Equals(_cluster.SelfAddress));
-            ReceiveAsync<ClusterEvent.MemberUp>(async m =>
-            {
-                await AddMember(m.Member);
-                BecomeTransmitting();
-            }, m => !m.Member.Address.Equals(_cluster.SelfAddress));
-            Receive<ClusterEvent.IMemberEvent>(_ => { }); // ignore
-            Receive<Terminated>(t => { _neighbors.Remove(t.ActorRef); });
-            ReceiveAny(_ => Stash.Stash());
-        }
-
-        private async Task AddMember(Member m)
-        {
-            var tries = 2;
-            while (tries > 0)
-                try
-                {
-                    var remoteRef = await Context.ActorSelection(m.Address + "/user/random")
-                        .ResolveOne(TimeSpan.FromSeconds(3));
-
-                    _neighbors.Add(remoteRef);
-                    Context.Watch(remoteRef);
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Context.GetInstrumentation().ActiveSpan?
-                        .Log($"Failed to resolve path for random actor on node {m.Address}")
-                        .Log($"Exception: {ex}");
-                    --tries;
-                }
-        }
-
-        private void BecomeTransmitting()
-        {
-            Become(Transmitting);
-            Stash.UnstashAll();
+           Transmitting();
         }
 
         public void Transmitting()
@@ -82,24 +34,17 @@ namespace Phobos.Samples.ClusterPing
             {
                 _log.Info("Received {0}", p);
                 var next = p.Next();
-                var start = DateTime.UtcNow;
 
                 Context.GetInstrumentation().ActiveSpan
                     .SetTag("foo", "bar");
 
-                var timing = Context.GetInstrumentation().Monitor.CreateTiming("process-time");
-
-                foreach (var m in _neighbors)
+                using (var timing = Context.GetInstrumentation().Monitor.StartTiming("process-time"))
                 {
-                    var value = ThreadLocalRandom.Current.Next(1, 10);
-                    if (value < 8)
-                        m.Tell(next);
-                    else // deadletter
-                        Context.ActorSelection("/user/fakeactor").Tell(next);
+                    foreach (var m in Enumerable.Range(0, ThreadLocalRandom.Current.Next(1, 4)))
+                    {
+                        _router.Tell(next);
+                    }
                 }
-
-                var stop = DateTime.UtcNow;
-                timing.Record((long) (stop - start).TotalMilliseconds);
             }, p => p.HasPings && p.Owned);
 
             Receive<RandomPing>(p =>
@@ -119,19 +64,6 @@ namespace Phobos.Samples.ClusterPing
                 // time to terminate this ping
                 Context.GetInstrumentation().ActiveSpan.Log("ping terminated");
             });
-
-            ReceiveAsync<ClusterEvent.MemberWeaklyUp>(async m =>
-            {
-                await AddMember(m.Member);
-            }, m => !m.Member.Address.Equals(_cluster.SelfAddress));
-
-            ReceiveAsync<ClusterEvent.MemberUp>(async m =>
-            {
-                await AddMember(m.Member);
-            }, m => !m.Member.Address.Equals(_cluster.SelfAddress));
-            
-            Receive<ClusterEvent.IMemberEvent>(_ => { }); // ignore
-            Receive<Terminated>(t => { _neighbors.Remove(t.ActorRef); });
         }
 
         protected override void PreStart()
@@ -143,8 +75,7 @@ namespace Phobos.Samples.ClusterPing
                 throw new ApplicationException("FATAL - MAILBOX MONITORING MUST BE ON");
             }
 
-            _cluster.Subscribe(Self, ClusterEvent.SubscriptionInitialStateMode.InitialStateAsEvents,
-                typeof(ClusterEvent.IMemberEvent));
+            _router = Context.ActorOf(Props.Empty.WithRouter(FromConfig.Instance), "router");
         }
 
         protected override void PostStop()
